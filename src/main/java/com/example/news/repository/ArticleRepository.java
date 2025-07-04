88390a9f5512da7f357e7333914264781d7056ce
HoanganhIUH <21015461.anh@student.iuh.edu.vn>
2025-07-04 17:16:32 +0700

    Add repository

---
+++ src/main/java/com/example/news/repository/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.example.news.repository;
+
+import com.example.news.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+} 
